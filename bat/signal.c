9fa50cf3c10b25828563ff236d1d2054e5293581
Lu, Han <han.lu@intel.com>
2015-09-02 10:59:13 +0800

    BAT: Add signal generator
    Add function that generates sine wavform through math lib.
    The waveform can be used as source for playback or analyses.
    
    Signed-off-by: Lu, Han <han.lu@intel.com>
    Signed-off-by: Liam Girdwood <liam.r.girdwood@intel.com>
    Signed-off-by: Bernard Gautier <bernard.gautier@intel.com>

---
+++ bat/signal.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013-2015 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#include "common.h"
+
+void generate_sine_wave(struct bat *bat, int length, void *buf, int max)
+{
+	static int i;
+	int k, c;
+	float sin_val[MAX_CHANNELS], sinus_f;
+
+	for (c = 0; c < bat->channels; c++)
+		sin_val[c] = (float) bat->target_freq[c] / (float) bat->rate;
+
+	for (k = 0; k < length; k++) {
+		for (c = 0; c < bat->channels; c++) {
+			sinus_f = sinf(i * 2.0 * M_PI * sin_val[c]) * max;
+			bat->convert_float_to_sample(sinus_f, buf);
+			buf += bat->sample_size;
+		}
+		i += 1;
+		if (i == bat->rate)
+			i = 0; /* Restart from 0 after one sine wave period */
+	}
+}
